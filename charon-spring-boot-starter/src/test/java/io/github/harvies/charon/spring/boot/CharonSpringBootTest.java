429f1904ec63b00cc2681b7eb51db15d2bb48d83
harvies <delverjw@gmail.com>
2020-07-10 00:14:42 +0800

    待修复

---
+++ charon-spring-boot-starter/src/test/java/io/github/harvies/charon/spring/boot/CharonSpringBootTest.java
@@ -0,0 +1,21 @@
+package io.github.harvies.charon.spring.boot;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.Resource;
+
+public class CharonSpringBootTest extends BaseTest {
+    @Value("${spring.jackson.time-zone:11}")
+    private String property;
+
+    @Resource
+    private Environment environment;
+
+    @Test
+    public void test() {
+        Assert.assertEquals("GMT+8", property);
+    }
+}
