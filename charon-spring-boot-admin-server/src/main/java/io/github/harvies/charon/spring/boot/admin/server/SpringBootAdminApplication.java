51485e67bf572e9f7f6715e2d9ce27e8bc4120ef
harvies <delverjw@gmail.com>
2020-07-18 22:09:27 +0800

    eureka

--- charon-spring-boot-admin-server/src/main/java/io/github/harvies/charon/spring/boot/admin/server/SpringBootAdminApplication.java
+++ charon-spring-boot-admin-server/src/main/java/io/github/harvies/charon/spring/boot/admin/server/SpringBootAdminApplication.java
@@ -2,14 +2,14 @@ package io.github.harvies.charon.spring.boot.admin.server;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
+@SpringBootApplication
 public class SpringBootAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
