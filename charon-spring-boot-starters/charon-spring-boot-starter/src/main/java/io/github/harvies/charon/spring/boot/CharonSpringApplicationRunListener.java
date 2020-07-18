0d4d51b4a91f152dcadfdfc7cd33f223fc52219d
harvies <delverjw@gmail.com>
2020-07-19 00:09:09 +0800

    apollo-starter默认命令空间

--- charon-spring-boot-starters/charon-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/CharonSpringApplicationRunListener.java
+++ charon-spring-boot-starters/charon-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/CharonSpringApplicationRunListener.java
@@ -1,10 +1,12 @@
 package io.github.harvies.charon.spring.boot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+@Slf4j
 public class CharonSpringApplicationRunListener implements SpringApplicationRunListener {
     private final SpringApplication application;
 
@@ -26,36 +28,36 @@ public class CharonSpringApplicationRunListener implements SpringApplicationRunL
 
     @Override
     public void starting() {
-        System.err.println("starting");
+        log.info("starting application:[{}]", application);
     }
 
     @Override
     public void environmentPrepared(ConfigurableEnvironment environment) {
-
+        log.info("environmentPrepared application:[{}] environment:[{}]", application, environment);
     }
 
     @Override
     public void contextPrepared(ConfigurableApplicationContext context) {
-
+        log.info("contextPrepared application:[{}] context:[{}]", application, context);
     }
 
     @Override
     public void contextLoaded(ConfigurableApplicationContext context) {
-
+        log.info("contextLoaded application:[{}] context:[{}]", application, context);
     }
 
     @Override
     public void started(ConfigurableApplicationContext context) {
-
+        log.info("started application:[{}] context:[{}]", application, context);
     }
 
     @Override
     public void running(ConfigurableApplicationContext context) {
-
+        log.info("running application:[{}] context:[{}]", application, context);
     }
 
     @Override
     public void failed(ConfigurableApplicationContext context, Throwable exception) {
-
+        log.info("failed application:[{}] context:[{}]", application, context);
     }
 }
