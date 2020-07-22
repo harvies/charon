173c11baea480acff6560698e77f23cf46bc5bc3
harvies <delverjw@gmail.com>
2020-07-23 01:17:13 +0800

    fastjson

--- charon-spring-boot-starters/charon-fastjson-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/fastjson/FastJsonAutoConfiguration.java
+++ charon-spring-boot-starters/charon-fastjson-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/fastjson/FastJsonAutoConfiguration.java
@@ -1,12 +1,32 @@
 package io.github.harvies.charon.spring.boot.fastjson;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(JSONObject.class)
 @EnableConfigurationProperties(FastJsonProperties.class)
 public class FastJsonAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FastJsonConfig fastJsonConfig() {
+        FastJsonConfig config = new FastJsonConfig();
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        return config;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter(FastJsonConfig config) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        converter.setFastJsonConfig(config);
+        return converter;
+    }
 }
--- charon-spring-boot-starters/charon-fastjson-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/fastjson/WebMvcConfig.java
+++ charon-spring-boot-starters/charon-fastjson-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/fastjson/WebMvcConfig.java
@@ -1,23 +1,20 @@
 package io.github.harvies.charon.spring.boot.fastjson;//package io.github.harvies.charon.spring.boot.gson;
 
-import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Resource
+    private FastJsonHttpMessageConverter fastJsonHttpMessageConverter;
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        //自定义配置...
-        FastJsonConfig config = new FastJsonConfig();
-        //config.set ...
-        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        converter.setFastJsonConfig(config);
-        converters.add(0, converter);
+        converters.add(0, fastJsonHttpMessageConverter);
     }
 }
