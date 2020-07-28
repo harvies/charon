9149f0e98d54acc50311fbfbb684984a0241a56f
harvies <delverjw@gmail.com>
2020-07-28 16:43:44 +0800

    server酱

--- charon-core/src/test/java/io/github/harvies/charon/util/RequestsTest.java
+++ charon-core/src/test/java/io/github/harvies/charon/util/RequestsTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author harvies
  */
@@ -26,5 +29,19 @@ public class RequestsTest {
     public void testSocksProxy() {
         RawResponse rawResponse = Requests.get("http://ip-api.com/json/?fields=query").timeout(1000 * 60 * 60).proxy(Proxies.socksProxy("127.0.0.1", 1080)).send();
         Assertions.assertNotNull(rawResponse.readToText());
+    }
+
+    /**
+     * server酱测试
+     */
+    @Test
+    public void ft() {
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("text", "测试消息标题");
+        paramMap.put("desp", "测试消息内容");
+        String text = Requests.post("https://sc.ftqq.com/SCU44485T1f79bf13e556ea84f657b63ff70fa3005c62778e1bc6f.send")
+                .params(paramMap)
+                .send().readToText();
+        Assertions.assertNotNull(text);
     }
 }
