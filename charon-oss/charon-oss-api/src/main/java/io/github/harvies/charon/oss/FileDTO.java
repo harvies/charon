be62ddf8f2ce3b9a82674c554c4249e7c79bf34f
harvies <delverjw@gmail.com>
2020-08-01 22:32:02 +0800

    新增无参构造，解决序列化无法实例化

--- charon-oss/charon-oss-api/src/main/java/io/github/harvies/charon/oss/FileDTO.java
+++ charon-oss/charon-oss-api/src/main/java/io/github/harvies/charon/oss/FileDTO.java
@@ -1,6 +1,7 @@
 package io.github.harvies.charon.oss;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Data
 @Accessors(chain = true)
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class FileDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
