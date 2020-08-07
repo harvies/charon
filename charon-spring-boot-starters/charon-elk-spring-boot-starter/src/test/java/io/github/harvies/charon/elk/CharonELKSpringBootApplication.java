ac079035cd58c70f1edc89a0dec0091e37b91014
harvies <delverjw@gmail.com>
2020-08-08 01:22:40 +0800

    elk

---
+++ charon-spring-boot-starters/charon-elk-spring-boot-starter/src/test/java/io/github/harvies/charon/elk/CharonELKSpringBootApplication.java
@@ -0,0 +1,11 @@
+package io.github.harvies.charon.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharonELKSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CharonELKSpringBootApplication.class, args);
+    }
+}
---
+++ charon-spring-boot-starters/charon-elk-spring-boot-starter/src/test/java/io/github/harvies/charon/elk/CharonELKSpringBootTest.java
@@ -0,0 +1,12 @@
+package io.github.harvies.charon.elk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CharonELKSpringBootTest extends BaseTest {
+    @Test
+    public void test() {
+        log.info("123123133121232132");
+    }
+}
