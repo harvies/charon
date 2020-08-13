b6ced52a4f2836b3e4dc742c0fc2c1ce502388cf
harvies <delverjw@gmail.com>
2020-08-13 23:30:01 +0800

    dubbo traceId

--- charon-spring-boot-starters/charon-dubbo-spring-boot-starter/src/main/java/io/github/harvies/charon/dubbo/DubboExtraPropertiesEnvironmentPostProcessor.java
+++ charon-spring-boot-starters/charon-dubbo-spring-boot-starter/src/main/java/io/github/harvies/charon/dubbo/DubboExtraPropertiesEnvironmentPostProcessor.java
@@ -21,7 +21,7 @@ public class DubboExtraPropertiesEnvironmentPostProcessor extends DubboDefaultPr
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         log.info("set dubbo properties begin");
         Properties properties = new Properties();
-        @Cleanup InputStream inputStream = new ClassPathResource("charon-dubbo.properties").getInputStream();
+        @Cleanup InputStream inputStream = new ClassPathResource("charon-dubbo/application.properties").getInputStream();
         properties.load(inputStream);
         PropertiesPropertySource propertiesPropertySource = new PropertiesPropertySource(DubboDefaultPropertiesEnvironmentPostProcessor.PROPERTY_SOURCE_NAME, properties);
         environment.getPropertySources().addLast(propertiesPropertySource);
