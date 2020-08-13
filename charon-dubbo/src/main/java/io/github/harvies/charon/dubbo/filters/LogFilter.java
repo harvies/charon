b6bb5431628b2b7f782c1bdad28d46c34968a5bf
harvies <delverjw@gmail.com>
2020-08-14 00:30:01 +0800

    dubbo traceId

--- charon-dubbo/src/main/java/io/github/harvies/charon/dubbo/filters/LogFilter.java
+++ charon-dubbo/src/main/java/io/github/harvies/charon/dubbo/filters/LogFilter.java
@@ -1,5 +1,6 @@
 package io.github.harvies.charon.dubbo.filters;
 
+import com.google.common.base.Stopwatch;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.dubbo.common.extension.Activate;
@@ -8,15 +9,15 @@ import org.apache.skywalking.apm.toolkit.trace.TraceContext;
 
 @Slf4j
 @Activate
-public class TestFilter implements Filter {
+public class LogFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         if (StringUtils.isBlank(invocation.getAttachment("traceId"))) {
             invocation.setAttachment("traceId", TraceContext.traceId());
         }
-        log.info("invoker begin:[{}] invocation:[{}] traceId:[{}]", invoker, invocation, invocation.getAttachment("traceId"));
         Result invoke = invoker.invoke(invocation);
-        log.info("invoker end:[{}] invocation:[{}] traceId:[{}]", invoker, invocation, invocation.getAttachment("traceId"));
+        log.info("invoker end:[{}] invocation:[{}] traceId:[{}] cast:[{}] ms", invoker, invocation, invocation.getAttachment("traceId"), stopwatch.elapsed().toMillis());
         return invoke;
     }
 }
