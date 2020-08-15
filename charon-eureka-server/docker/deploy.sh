d43d9c080a5b67e5fc5efb03e3cb240c1d657c45
harvies <delverjw@gmail.com>
2020-08-15 22:48:14 +0800

    jenkins

--- charon-eureka-server/docker/deploy.sh
+++ charon-eureka-server/docker/deploy.sh
@@ -2,12 +2,12 @@
 
 docker stack  deploy -c  docker-stack.yml charon-eureka-server-cluster
 
-docker service update charon-eureka-server_eureka1 --force
+docker service update charon-eureka-server-cluster_eureka1 --force
 
 sleep 10s
 
-docker service update charon-eureka-server_eureka2 --force
+docker service update charon-eureka-server-cluster_eureka2 --force
 
 sleep 10s
 
-docker service update charon-eureka-server_eureka3 --force
+docker service update charon-eureka-server-cluster_eureka3 --force
