8fe217525e8e1b8918390aa786be3fff18d88108
harvies <delverjw@gmail.com>
2020-08-21 23:05:57 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-dubbo-spring-boot-starter/pom.xml
    	modified:   charon-spring-boot-starters/charon-dubbo-spring-boot-starter/src/test/java/io/github/harvies/charon/dubbo/producer/EchoServiceImpl.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-spring-boot-starters/charon-dubbo-spring-boot-starter/src/test/java/io/github/harvies/charon/dubbo/producer/EchoServiceImpl.java
+++ charon-spring-boot-starters/charon-dubbo-spring-boot-starter/src/test/java/io/github/harvies/charon/dubbo/producer/EchoServiceImpl.java
@@ -1,9 +1,9 @@
 package io.github.harvies.charon.dubbo.producer;
 
 import io.github.harvies.charon.dubbo.api.EchoService;
-import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.config.annotation.DubboService;
 
-@Service
+@DubboService
 public class EchoServiceImpl implements EchoService {
     @Override
     public String echo(String content) {
