95949dd15d80b9c38a3047b7c6179eaca85d92b6
harvies <delverjw@gmail.com>
2020-08-23 23:33:01 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/controller/monitor/HealthController.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/controller/monitor/HealthController.java
+++ charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/controller/monitor/HealthController.java
@@ -3,12 +3,13 @@ package io.github.harvies.charon.spring.boot.web.controller.monitor;
 import io.github.harvies.charon.spring.boot.web.result.ResultDTO;
 import io.github.harvies.charon.spring.boot.web.result.Results;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/health")
+@RequestMapping(value = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HealthController {
 
     @RequestMapping("/status")
