93aa636057942485e030f53dbe91f9f5b7e1a503
harvies <delverjw@gmail.com>
2020-08-30 04:44:11 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/controller/monitor/HealthController.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/controller/monitor/HealthController.java
+++ charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/controller/monitor/HealthController.java
@@ -2,6 +2,7 @@ package io.github.harvies.charon.spring.boot.web.controller.monitor;
 
 import io.github.harvies.charon.spring.boot.web.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class HealthController {
 
     public String status = Constants.SUCCESS;
 
-    @RequestMapping("/status")
+    @RequestMapping(value = "/status", produces = MediaType.TEXT_PLAIN_VALUE)
     public String status() {
         return status;
     }
