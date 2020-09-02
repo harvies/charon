59679c391c36c4ced9ff45d90319b5b73249b774
harvies <delverjw@gmail.com>
2020-09-02 18:27:26 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-core/src/test/java/io/github/harvies/charon/util/ImageWatermarkTest.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-core/src/test/java/io/github/harvies/charon/util/ImageWatermarkTest.java
+++ charon-core/src/test/java/io/github/harvies/charon/util/ImageWatermarkTest.java
@@ -8,8 +8,8 @@ class ImageWatermarkTest {
 
     @Test
     void markImageBySingleIcon() {
-        String icon = FileUtils.getCurrentUserHomePath() + "/Downloads/baidu.png";
-        String source = FileUtils.getCurrentUserHomePath() + "/Downloads/img.jpeg";
+        String icon = FileUtils.getCurrentUserHomePath() + "/Downloads/icon.png";
+        String source = FileUtils.getCurrentUserHomePath() + "/Downloads/520dcdcf-932c-4d19-bfc8-b7b00d592f8f.jpeg";
         String output = FileUtils.getCurrentUserHomePath() + "/Downloads/";
         ImageWatermark.Param.ParamBuilder paramBuilder = ImageWatermark.Param.builder()
                 .icon(icon)
@@ -19,5 +19,6 @@ class ImageWatermarkTest {
                 .imageName("aaa")
                 .imageType("jpeg");
         ImageWatermark.markImageBySingleIcon(paramBuilder.build());
+        PicUtils.compressPicForScale(output+"aaa.jpeg",output+"aaa_1.jpeg",500,0.8,999999999,99999999);
     }
 }
