092f53c1ee9befb092919618469fa31847944895
harvies <delverjw@gmail.com>
2020-09-12 22:26:56 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   docker/adminer/designs/brade/adminer.css
    	new file:   docker/adminer/designs/bueltge/adminer.css
    	new file:   docker/adminer/designs/cvicebni-ubor/adminer.css
    	new file:   docker/adminer/designs/esterka/adminer.css
    	new file:   docker/adminer/designs/flat/adminer.css
    	new file:   docker/adminer/designs/galkaev/adminer.css
    	new file:   docker/adminer/designs/haeckel/adminer.css
    	new file:   docker/adminer/designs/hever/adminer.css
    	new file:   docker/adminer/designs/jukin/adminer.css
    	new file:   docker/adminer/designs/kahi/adminer.css
    	new file:   docker/adminer/designs/konya/adminer.css
    	new file:   docker/adminer/designs/lucas-sandery/adminer.css
    	new file:   docker/adminer/designs/mancave-hever/adminer.css
    	new file:   docker/adminer/designs/mancave/adminer.css
    	new file:   docker/adminer/designs/mvt/adminer.css
    	new file:   docker/adminer/designs/nette/adminer.css
    	new file:   docker/adminer/designs/ng9/adminer.css
    	new file:   docker/adminer/designs/nicu/adminer.css
    	new file:   docker/adminer/designs/pappu687/adminer.css
    	new file:   docker/adminer/designs/paranoiq/adminer.css
    	new file:   docker/adminer/designs/pepa-linha/adminer.css
    	new file:   docker/adminer/designs/pilot/adminer.css
    	new file:   docker/adminer/designs/pokorny/adminer.css
    	new file:   docker/adminer/designs/price/adminer.css
    	new file:   docker/adminer/designs/readme.txt
    	new file:   docker/adminer/designs/rmsoft/adminer.css
    	new file:   docker/adminer/designs/rmsoft_blue/adminer.css
    	modified:   docker/adminer/index.php
    	new file:   docker/adminer/plugin-loader.php
    	new file:   docker/adminer/plugins/database-hide.php
    	new file:   docker/adminer/plugins/designs.php
    	new file:   docker/adminer/plugins/dump-alter.php
    	new file:   docker/adminer/plugins/dump-bz2.php
    	new file:   docker/adminer/plugins/dump-date.php
    	new file:   docker/adminer/plugins/dump-json.php
    	new file:   docker/adminer/plugins/dump-php.php
    	new file:   docker/adminer/plugins/dump-xml.php
    	new file:   docker/adminer/plugins/dump-zip.php
    	new file:   docker/adminer/plugins/edit-calendar.php
    	new file:   docker/adminer/plugins/edit-foreign.php
    	new file:   docker/adminer/plugins/edit-textarea.php
    	new file:   docker/adminer/plugins/email-table.php
    	new file:   docker/adminer/plugins/enum-option.php
    	new file:   docker/adminer/plugins/enum-types.php
    	new file:   docker/adminer/plugins/file-upload.php
    	new file:   docker/adminer/plugins/foreign-system.php
    	new file:   docker/adminer/plugins/frames.php
    	new file:   docker/adminer/plugins/json-column.php
    	new file:   docker/adminer/plugins/login-ip.php
    	new file:   docker/adminer/plugins/login-otp.php
    	new file:   docker/adminer/plugins/login-password-less.php
    	new file:   docker/adminer/plugins/login-servers.php
    	new file:   docker/adminer/plugins/login-ssl.php
    	new file:   docker/adminer/plugins/login-table.php
    	new file:   docker/adminer/plugins/master-slave.php
    	new file:   docker/adminer/plugins/plugin.php
    	new file:   docker/adminer/plugins/pretty-json-column.php
    	new file:   docker/adminer/plugins/readme.txt
    	new file:   docker/adminer/plugins/slugify.php
    	new file:   docker/adminer/plugins/sql-log.php
    	new file:   docker/adminer/plugins/struct-comments.php
    	new file:   docker/adminer/plugins/table-indexes-structure.php
    	new file:   docker/adminer/plugins/table-structure.php
    	new file:   docker/adminer/plugins/tables-filter.php
    	new file:   docker/adminer/plugins/tinymce.php
    	new file:   docker/adminer/plugins/translation.php
    	new file:   docker/adminer/plugins/version-noverify.php
    	new file:   docker/adminer/plugins/wymeditor.php

--- docker/adminer/index.php
+++ docker/adminer/index.php
@@ -1,32 +1,43 @@
 <?php
-function adminer_object() {
-    // required to run any plugin
-    include_once "./plugins/plugin.php";
-    
-    // autoloader
-    foreach (glob("plugins/*.php") as $filename) {
-        include_once "./$filename";
-    }
-    
-    $plugins = array(
-        // specify enabled plugins here
-        new AdminerDumpXml,
-        new AdminerTinymce,
-        new AdminerFileUpload("data/"),
-        new AdminerSlugify,
-        new AdminerTranslation,
-        new AdminerForeignSystem,
-    );
-    
-    /* It is possible to combine customization and plugins:
-    class AdminerCustomization extends AdminerPlugin {
-    }
-    return new AdminerCustomization($plugins);
-    */
-    
-    return new AdminerPlugin($plugins);
+namespace docker {
+	function adminer_object() {
+		require_once('plugins/plugin.php');
+
+		class Adminer extends \AdminerPlugin {
+			function _callParent($function, $args) {
+				if ($function === 'loginForm') {
+					ob_start();
+					$return = \Adminer::loginForm();
+					$form = ob_get_clean();
+
+					echo str_replace('name="auth[server]" value="" title="hostname[:port]"', 'name="auth[server]" value="'.($_ENV['ADMINER_DEFAULT_SERVER'] ?: 'db').'" title="hostname[:port]"', $form);
+
+					return $return;
+				}
+
+				return parent::_callParent($function, $args);
+			}
+		}
+
+		$plugins = [];
+		foreach (glob('plugins-enabled/*.php') as $plugin) {
+			$plugins[] = require($plugin);
+		}
+
+		return new Adminer($plugins);
+	}
 }
 
-// include original Adminer or Adminer Editor
-include "./adminer.php";
-?>
+namespace {
+	if (basename($_SERVER['DOCUMENT_URI']) === 'adminer.css' && is_readable('adminer.css')) {
+		header('Content-Type: text/css');
+		readfile('adminer.css');
+		exit;
+	}
+
+	function adminer_object() {
+		return \docker\adminer_object();
+	}
+
+	require('adminer.php');
+}
