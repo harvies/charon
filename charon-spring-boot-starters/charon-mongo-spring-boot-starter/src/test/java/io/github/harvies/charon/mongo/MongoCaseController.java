b9898eba8bbc65f1b8359f5c46457ab4b48d33cd
harvies <cr0n6e@gmail.com>
2020-09-16 23:28:01 +0800

    On branch master Your branch is up to date with 'github/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-spring-boot-starters/charon-mongo-spring-boot-starter/src/test/java/io/github/harvies/charon/mongo/MongoCaseController.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-mongo-spring-boot-starter/pom.xml
    	modified:   charon-spring-boot-starters/charon-mongo-spring-boot-starter/src/test/java/io/github/harvies/charon/mongo/CharonMongoSpringBootTest.java
    	modified:   charon-spring-boot-starters/charon-mongo-spring-boot-starter/src/test/java/io/github/harvies/charon/mongo/MongoCaseController.java
    	modified:   charon-spring-boot-starters/charon-mongo-spring-boot-starter/src/test/resources/application.properties

--- charon-spring-boot-starters/charon-mongo-spring-boot-starter/src/test/java/io/github/harvies/charon/mongo/MongoCaseController.java
+++ charon-spring-boot-starters/charon-mongo-spring-boot-starter/src/test/java/io/github/harvies/charon/mongo/MongoCaseController.java
@@ -1,27 +1,39 @@
 package io.github.harvies.charon.mongo;
 
 import com.mongodb.client.MongoDatabase;
+import io.github.harvies.charon.spring.boot.web.result.ResultDTO;
+import io.github.harvies.charon.spring.boot.web.result.Results;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * @author harvies
+ */
+@RestController
+@RequestMapping(value = "/mongo", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
-public class CharonMongoSpringBootTest extends BaseTest {
+public class MongoCaseController {
+
 
     @Resource
+    //io.github.harvies.charon.mongo.MongoAutoConfiguration.init执行晚于bean注入
     @Lazy
     private List<MongoTemplate> mongoTemplateList;
 
-    @Test
-    public void listCollectionNames() {
+    @RequestMapping("/case")
+    public ResultDTO<Boolean> mongodb() {
         for (MongoTemplate mongoTemplate : mongoTemplateList) {
             MongoDatabase mongoDatabase = mongoTemplate.getDb();
             mongoDatabase.listCollectionNames().forEach(s -> log.info("{} dbName:[{}] collectionNames:[{}]", mongoTemplate.hashCode(), mongoDatabase.getName(), s));
             log.info("--------------------");
         }
+        return Results.success(true);
     }
 }
