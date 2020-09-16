817994ce39f08b1e3484691a55d49c68096f100f
harvies <cr0n6e@gmail.com>
2020-09-16 13:02:49 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   docker/alpine/build.sh
    	modified:   docker/jdk/build.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- docker/alpine/build.sh
+++ docker/alpine/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache  -t registry.cn-hangzhou.aliyuncs.com/harvies/alpine:latest .
+docker build --pull --no-cache -t registry.cn-hangzhou.aliyuncs.com/harvies/alpine:latest .
--- docker/jdk/build.sh
+++ docker/jdk/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache  -t registry.cn-hangzhou.aliyuncs.com/harvies/jdk:latest .
+docker build --pull --no-cache  -t registry.cn-hangzhou.aliyuncs.com/harvies/jdk:latest .
