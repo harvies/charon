97f5af22d0b5b59f1d6fe5a5f100f7c7d8073c91
harvies <cr0n6e@gmail.com>
2020-09-22 17:25:13 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	deleted:    charon-core/src/main/java/io/github/harvies/charon/util/ClassUtils.java
    	deleted:    charon-core/src/test/java/io/github/harvies/charon/util/ClassUtilsTest.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-core/src/main/java/io/github/harvies/charon/util/MavenUtils.java
    	modified:   charon-core/src/test/java/io/github/harvies/charon/util/MavenUtilsTest.java

--- charon-core/src/main/java/io/github/harvies/charon/util/MavenUtils.java
+++ charon-core/src/main/java/io/github/harvies/charon/util/MavenUtils.java
@@ -3,12 +3,17 @@ package io.github.harvies.charon.util;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import io.github.harvies.charon.model.artifact.MavenArtifact;
 import lombok.extern.slf4j.Slf4j;
 import net.dongliu.requests.Requests;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.artifact.versioning.ComparableVersion;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -71,5 +76,107 @@ public class MavenUtils {
      */
     public static int compareVersion(String v1, String v2) {
         return new ComparableVersion(v1).compareTo(new ComparableVersion(v2));
+    }
+
+    /**
+     * 某版本的包中是否存在某类
+     *
+     * @param className
+     * @param mavenArtifact
+     * @return
+     */
+    public static boolean exists(String className, MavenArtifact mavenArtifact) {
+        return getAllClassNameListByMavenArtifact(mavenArtifact).contains(className);
+    }
+
+    //com.mongodb.LazyDBObject
+    public static void setArtifactInfo(String className, MavenArtifact mavenArtifact) {
+        ArrayList<MavenArtifact> aliasArtifactList = new ArrayList<>(mavenArtifact.getAliasArtifactList());
+        aliasArtifactList.add(0, mavenArtifact);
+        for (MavenArtifact artifact : aliasArtifactList) {
+            List<String> versionList = MavenUtils.getVersionList(artifact.getGroupId(), artifact.getArtifactId());
+            for (String version : versionList) {
+                try {
+                    List<String> allClassNameListByMavenArtifact = getAllClassNameListByMavenArtifact(artifact.getGroupId(), artifact.getArtifactId(), version);
+                    if (mavenArtifact.getFirstAddedArtifact() == null && allClassNameListByMavenArtifact.contains(className)) {
+                        MavenArtifact firstAddedArtifact = new MavenArtifact()
+                                .setGroupId(artifact.getGroupId())
+                                .setArtifactId(artifact.getArtifactId())
+                                .setVersion(version)
+                                .setVersionList(versionList);
+                        mavenArtifact.setFirstAddedArtifact(firstAddedArtifact);
+                    }
+                    boolean remove = mavenArtifact.getFirstAddedArtifact() != null
+                            && !allClassNameListByMavenArtifact.contains(className)
+                            && MavenUtils.compareVersion(version, mavenArtifact.getFirstAddedArtifact().getVersion()) > 0;
+                    if (remove) {
+                        MavenArtifact firstRemovedArtifact = new MavenArtifact()
+                                .setGroupId(artifact.getGroupId())
+                                .setArtifactId(artifact.getArtifactId())
+                                .setVersion(version)
+                                .setVersionList(versionList);
+                        mavenArtifact.setFirstRemovedArtifact(firstRemovedArtifact);
+                    }
+                    if (mavenArtifact.getFirstAddedArtifact() != null && mavenArtifact.getFirstRemovedArtifact() != null) {
+                        break;
+                    }
+                } catch (Exception e) {
+                    log.warn("set setArtifactInfo error mavenArtifact:[{}] version:[{}]", mavenArtifact, version);
+                }
+            }
+        }
+        aliasArtifactList.remove(mavenArtifact);
+    }
+
+    public static List<String> getNewClassList(MavenArtifact oldArtifact, List<MavenArtifact> newArtifactList) {
+        return getNewClassList(Collections.singletonList(oldArtifact), newArtifactList);
+    }
+
+    public static List<String> getNewClassList(List<MavenArtifact> oldArtifactList, List<MavenArtifact> newArtifactList) {
+        List<String> allOldClassNameListByMavenArtifact = Lists.newArrayList();
+        List<String> allNewClassNameListByMavenArtifact = Lists.newArrayList();
+        for (MavenArtifact oldArtifact : oldArtifactList) {
+            allOldClassNameListByMavenArtifact.addAll(getAllClassNameListByMavenArtifact(oldArtifact));
+        }
+        for (MavenArtifact newArtifact : newArtifactList) {
+            allNewClassNameListByMavenArtifact.addAll(getAllClassNameListByMavenArtifact(newArtifact));
+        }
+        allNewClassNameListByMavenArtifact.removeAll(allOldClassNameListByMavenArtifact);
+        return allNewClassNameListByMavenArtifact;
+    }
+
+    public static List<String> getNewClassList(MavenArtifact oldArtifact, MavenArtifact newArtifact) {
+        return getNewClassList(Collections.singletonList(oldArtifact), Collections.singletonList(newArtifact));
+    }
+
+    public static List<String> getOldClassList(List<MavenArtifact> oldArtifactList, List<MavenArtifact> newArtifactList) {
+        return getNewClassList(newArtifactList, oldArtifactList);
+    }
+
+    public static List<String> getOldClassList(MavenArtifact oldArtifact, MavenArtifact newArtifact) {
+        return getNewClassList(newArtifact, oldArtifact);
+    }
+
+    public static List<String> getOldClassList(MavenArtifact oldArtifact, List<MavenArtifact> newArtifactList) {
+        return getNewClassList(newArtifactList, Collections.singletonList(oldArtifact));
+    }
+
+    public static List<String> getAllClassNameListByMavenArtifact(MavenArtifact mavenArtifact) {
+        return getAllClassNameListByMavenArtifact(mavenArtifact.getGroupId(), mavenArtifact.getArtifactId(), mavenArtifact.getVersion());
+    }
+
+    public static List<String> getAllClassNameListByMavenArtifact(String groupId, String artifactId, String version) {
+        String jarFileUrl = MavenUtils.getJarFileUrl(groupId, artifactId, version);
+        File file = new File(FileUtils.getTmpDir() + RandomUtils.uuid());
+        try {
+            byte[] bytes = Requests.get(jarFileUrl).send().readToBytes();
+            FileUtils.writeByteArrayToFile(file, bytes);
+            return JarUtils.getClassNameListByJarFile(file);
+        } catch (IOException e) {
+            log.info("getAllClassNameList error groupId:[{}] artifactId:[{}] version:[{}]", groupId, artifactId, version);
+        } finally {
+            file.delete();
+        }
+        return Collections.emptyList();
     }
 }
--- charon-core/src/test/java/io/github/harvies/charon/util/MavenUtilsTest.java
+++ charon-core/src/test/java/io/github/harvies/charon/util/MavenUtilsTest.java
@@ -1,8 +1,10 @@
 package io.github.harvies.charon.util;
 
+import io.github.harvies.charon.model.artifact.MavenArtifact;
 import net.dongliu.requests.Requests;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,6 +13,86 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MavenUtilsTest {
+
+
+    private MavenArtifact oldMavenArtifact = new MavenArtifact()
+            .setGroupId("org.mongodb")
+            .setArtifactId("mongo-java-driver")
+            .setVersion("3.7.0");
+
+    private MavenArtifact currentMavenArtifact = new MavenArtifact()
+            .setGroupId("org.mongodb")
+            .setArtifactId("mongo-java-driver")
+            .setVersion("3.8.0");
+
+    private MavenArtifact newMavenArtifact = new MavenArtifact()
+            .setGroupId("org.mongodb")
+            .setArtifactId("mongodb-driver-core")
+            .setVersion("4.0.0");
+
+    private List<MavenArtifact> newMavenArtifactList = Arrays.asList(newMavenArtifact, new MavenArtifact()
+            .setGroupId("org.mongodb")
+            .setArtifactId("mongodb-driver-sync")
+            .setVersion("4.0.0"), new MavenArtifact()
+            .setGroupId("org.mongodb")
+            .setArtifactId("bson")
+            .setVersion("4.0.0"));
+
+    @Test
+    void getAllClassNameList() {
+        MavenArtifact mavenArtifact = new MavenArtifact()
+                .setGroupId("org.mongodb")
+                .setArtifactId("mongo-drive-driver")
+                .setVersion("3.8.0");
+        List<String> allClassNameList = MavenUtils.getAllClassNameListByMavenArtifact(mavenArtifact);
+        System.err.println(allClassNameList.size());
+        allClassNameList.forEach(s -> System.err.println(s + "\r\n"));
+    }
+
+    @Test
+    void getNewClassList() {
+        List<String> newClassList = MavenUtils.getNewClassList(currentMavenArtifact, newMavenArtifactList);
+        System.err.println(newClassList.size());
+        newClassList.forEach(s -> System.err.println(s + "\r\n"));
+    }
+
+    @Test
+    void getOldClassList() {
+        List<String> oldClassList = MavenUtils.getOldClassList(currentMavenArtifact, newMavenArtifactList);
+        System.err.println(oldClassList.size());
+        oldClassList.forEach(s -> System.err.println(s + "\r\n"));
+    }
+
+    @Test
+    void getNewOldClassList() {
+        List<String> newClassList = MavenUtils.getNewClassList(oldMavenArtifact, currentMavenArtifact);
+        List<String> oldClassList = MavenUtils.getOldClassList(currentMavenArtifact, newMavenArtifactList);
+        newClassList.retainAll(oldClassList);
+        System.err.println(newClassList.size());
+        newClassList.forEach(s -> System.err.println(s + "\r\n"));
+    }
+
+    @Test
+    void setArtifactInfo() {
+        MavenArtifact mavenArtifact = new MavenArtifact()
+                .setGroupId("org.mongodb")
+                .setArtifactId("mongodb-java-drive")
+                .setAliasArtifactList(
+                        Arrays.asList(
+                                new MavenArtifact().setGroupId("org.mongodb").setArtifactId("mongodb-driver-sync"),
+                                new MavenArtifact().setGroupId("org.mongodb").setArtifactId("mongodb-driver-core"),
+                                new MavenArtifact().setGroupId("org.mongodb").setArtifactId("bson")
+                        )
+                );
+        MavenUtils.setArtifactInfo("com.mongodb.operation.AggregateOperationImpl", mavenArtifact);
+        System.err.println(JsonUtils.toJSONString(mavenArtifact));
+    }
+
+    @Test
+    void exists() {
+        boolean exists = MavenUtils.exists("com.mongodb.operation.AggregateOperationImpl", currentMavenArtifact);
+        System.err.println(exists);
+    }
 
     @Test
     public void getVersionList() {
