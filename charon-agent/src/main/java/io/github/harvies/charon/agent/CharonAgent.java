58d3e6492bf22da41a56b01d29dc0e9bc6bf706e
harvies <cr0n6e@gmail.com>
2020-09-22 13:06:23 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-agent/src/main/java/io/github/harvies/charon/agent/TimeInterceptor.java
    	new file:   charon-agent/src/main/java/io/github/harvies/charon/agent/TimeListener.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-agent/src/main/java/io/github/harvies/charon/agent/CharonAgent.java
    	modified:   charon-agent/src/main/java/io/github/harvies/charon/agent/TimeInterceptor.java
    	modified:   charon-agent/src/main/java/io/github/harvies/charon/agent/TimeListener.java

--- charon-agent/src/main/java/io/github/harvies/charon/agent/CharonAgent.java
+++ charon-agent/src/main/java/io/github/harvies/charon/agent/CharonAgent.java
@@ -1,5 +1,9 @@
 package io.github.harvies.charon.agent;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+
 import java.lang.instrument.Instrumentation;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -16,12 +20,40 @@ public class CharonAgent {
 //        inst.addTransformer(new CharonTransformer());
 //        System.out.println("=========premain方法执行完毕========");
         init();
+
+        addInterceptor(inst);
+    }
+
+    private static void addInterceptor(Instrumentation inst) {
+        installTimeInterceptor(inst);
+    }
+
+    private static void installTimeInterceptor(Instrumentation inst) {
+        AgentBuilder.Transformer transformer = (builder, typeDescription, classLoader, module) -> {
+            return builder
+                    .method(ElementMatchers.any()) // 拦截任意方法
+                    .intercept(MethodDelegation.to(TimeInterceptor.class)); // 委托
+        };
+
+        AgentBuilder.Listener listener = new TimeListener();
+
+        new AgentBuilder
+                .Default()
+                .type(
+                        ElementMatchers.nameStartsWith("io.github.harvies.blazars")
+                                .or(ElementMatchers.nameStartsWith("io.github.harvies.charon"))
+                ) // 指定需要拦截的类
+                .transform(transformer)
+                .with(listener)
+                .installOn(inst);
     }
 
     public static void init() {
         Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
             Metric.printMemoryInfo();
             Metric.printGCInfo();
-        }, 0, 5000, TimeUnit.MILLISECONDS);
+        }, 0, 30, TimeUnit.SECONDS);
+
     }
+
 }
---
+++ charon-agent/src/main/java/io/github/harvies/charon/agent/TimeListener.java
@@ -0,0 +1,33 @@
+package io.github.harvies.charon.agent;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.utility.JavaModule;
+
+public class TimeListener implements AgentBuilder.Listener {
+    @Override
+    public void onDiscovery(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+
+    }
+
+    @Override
+    public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded, DynamicType dynamicType) {
+
+    }
+
+    @Override
+    public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded) {
+
+    }
+
+    @Override
+    public void onError(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded, Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+
+    }
+}
