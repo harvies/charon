f2f86026eac51dd80aeae411b65657aac2040849
harvies <cr0n6e@gmail.com>
2020-09-28 16:06:47 +0800

    1

---
+++ charon-agent/src/test/java/io/github/harvies/charon/agent/BootstrapTest.java
@@ -0,0 +1,12 @@
+package io.github.harvies.charon.agent;
+
+import org.junit.jupiter.api.Test;
+
+public class BootstrapTest {
+    @Test
+    public void test() throws InterruptedException {
+        System.out.println("starting");
+        Thread.sleep(100);
+        System.out.println("started");
+    }
+}
