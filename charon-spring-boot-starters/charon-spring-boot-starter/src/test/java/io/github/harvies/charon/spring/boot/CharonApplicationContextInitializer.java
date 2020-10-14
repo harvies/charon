b73af001c487d041b2780c9b481671dc553c6cee
harvies <cr0n6e@gmail.com>
2020-10-14 17:53:49 +0800

    On branch master Your branch is up to date with 'github/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-spring-boot-starters/charon-spring-boot-starter/src/test/java/io/github/harvies/charon/spring/boot/CharonApplicationContextInitializer.java
    	new file:   charon-spring-boot-starters/charon-spring-boot-starter/src/test/resources/META-INF/spring.factories
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-spring-boot-starter/src/test/java/io/github/harvies/charon/spring/boot/CharonApplicationContextInitializer.java
    	modified:   charon-spring-boot-starters/charon-spring-boot-starter/src/test/resources/META-INF/spring.factories

---
+++ charon-spring-boot-starters/charon-spring-boot-starter/src/test/java/io/github/harvies/charon/spring/boot/CharonApplicationContextInitializer.java
@@ -0,0 +1,11 @@
+package io.github.harvies.charon.spring.boot;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class CharonApplicationContextInitializer implements ApplicationContextInitializer {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+
+    }
+}
