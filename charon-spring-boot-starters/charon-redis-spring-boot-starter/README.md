ff0a80c4c72de710da5145d5b755660e4c9e26c4
harvies <cr0n6e@gmail.com>
2020-10-16 17:26:54 +0800

    On branch master Your branch is up to date with 'github/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	deleted:    charon-spring-boot-starters/charon-redis-spring-boot-starter/src/main/java/io/github/harvies/charon/redis/RedisAutoConfiguration.java
    	deleted:    charon-spring-boot-starters/charon-redis-spring-boot-starter/src/main/resources/META-INF/spring.factories
    	deleted:    charon-spring-boot-starters/charon-redis-spring-boot-starter/src/test/java/io/github/harvies/charon/redis/CharonRedisSpringBootTest.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-redis-spring-boot-starter/README.md
    	modified:   charon-spring-boot-starters/charon-redis-spring-boot-starter/pom.xml
    	modified:   charon-spring-boot-starters/charon-redis-spring-boot-starter/src/test/java/io/github/harvies/charon/redis/RedissonClientSpringBootTest.java

--- charon-spring-boot-starters/charon-redis-spring-boot-starter/README.md
+++ charon-spring-boot-starters/charon-redis-spring-boot-starter/README.md
@@ -1,34 +1,3 @@
 ## 介绍
 
-整合redis-spring-boot(redis常用命令)和Redisson(分布式锁等)
-
-如需使用Redisson，需引入
-```xml
-        <dependency>
-            <groupId>org.redisson</groupId>
-            <artifactId>redisson</artifactId>
-        </dependency>
-```
-
-## 单机模式配置
-```properties
-charon.env=dev
-charon.application.name=charon-redis
-spring.redis.client-name=${charon.application.name}
-spring.redis.host=
-spring.redis.port=
-spring.redis.password=
-spring.redis.database=0
-spring.redis.timeout=3s
-```
-
-## cluster模式配置
-```properties
-charon.env=dev
-charon.application.name=charon-redis
-spring.redis.client-name=${charon.application.name}
-spring.redis.cluster.nodes=host1:port1,host2:port2,host3:port3
-spring.redis.database=0
-spring.redis.timeout=3s
-
-```
+Redisson
--- charon-spring-boot-starters/charon-redis-spring-boot-starter/src/test/java/io/github/harvies/charon/redis/RedissonClientSpringBootTest.java
+++ charon-spring-boot-starters/charon-redis-spring-boot-starter/src/test/java/io/github/harvies/charon/redis/RedissonClientSpringBootTest.java
@@ -3,9 +3,12 @@ package io.github.harvies.charon.redis;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.SortOrder;
 
 import javax.annotation.Resource;
+import java.util.Set;
 
 public class RedissonClientSpringBootTest extends BaseTest {
 
@@ -21,5 +24,22 @@ public class RedissonClientSpringBootTest extends BaseTest {
     public void test() {
         long l = redissonClient.getAtomicLong("test-long").addAndGet(1);
         Assertions.assertEquals(1, l);
+    }
+
+    @Test
+    public void test2() {
+        Object o = redissonClient.getMap("testMap").addAndGet("key1", 1L);
+        System.out.println(o);
+    }
+
+    @Test
+    public void testSortSet() {
+        RScoredSortedSet<Object> sortSet = redissonClient.getScoredSortedSet("testSortSet");
+        sortSet.delete();
+        sortSet.addScore("key1", 2);
+        sortSet.addScore("key3", 1);
+        sortSet.addScore("key2", 3);
+        Set<Object> objects = sortSet.readSort("score", SortOrder.DESC, 0, 2);
+        System.out.println(objects);//[key2, key1]
     }
 }
