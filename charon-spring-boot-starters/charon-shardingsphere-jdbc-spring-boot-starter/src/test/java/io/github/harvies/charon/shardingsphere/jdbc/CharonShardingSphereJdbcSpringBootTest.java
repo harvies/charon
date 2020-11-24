7aec42de2f81bb67ecdd18b86e4a1a73ad03b0e3
harvies <cr0n6e@gmail.com>
2020-11-24 13:52:20 +0800

    1

--- charon-spring-boot-starters/charon-shardingsphere-jdbc-spring-boot-starter/src/test/java/io/github/harvies/charon/shardingsphere/jdbc/CharonShardingSphereJdbcSpringBootTest.java
+++ charon-spring-boot-starters/charon-shardingsphere-jdbc-spring-boot-starter/src/test/java/io/github/harvies/charon/shardingsphere/jdbc/CharonShardingSphereJdbcSpringBootTest.java
@@ -3,6 +3,7 @@ package io.github.harvies.charon.shardingsphere.jdbc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -12,9 +13,16 @@ public class CharonShardingSphereJdbcSpringBootTest extends BaseTest {
 
     @Resource
     private DataSource dataSource;
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     public void test() {
         Assertions.assertNotNull(dataSource);
+    }
+
+    @Test
+    public void jdbcTemplate() {
+        System.out.println(jdbcTemplate.queryForList("show databases"));
     }
 }
