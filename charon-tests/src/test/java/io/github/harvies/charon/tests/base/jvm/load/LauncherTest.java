b618e8ebbe1c8073bcb1f4f3a67065d082e7d174
harvies <cr0n6e@gmail.com>
2020-12-04 15:29:20 +0800

    On branch master Your branch is up to date with 'github/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-tests/src/test/java/io/github/harvies/charon/tests/base/jdk/concurrent/atomic/AtomicFieldUpdaterExample.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-dependencies/pom.xml
    	modified:   charon-tests/src/test/java/io/github/harvies/charon/tests/base/jdk/concurrent/atomic/AtomicFieldUpdaterExample.java
    	modified:   charon-tests/src/test/java/io/github/harvies/charon/tests/base/jvm/load/LauncherTest.java

--- charon-tests/src/test/java/io/github/harvies/charon/tests/base/jvm/load/LauncherTest.java
+++ charon-tests/src/test/java/io/github/harvies/charon/tests/base/jvm/load/LauncherTest.java
@@ -1,10 +1,11 @@
-package io.github.harvies.charon.tests.base.jvm.load;
-
-import sun.misc.Launcher;
-
-public class LauncherTest {
-    public static void main(String[] args) {
-        ClassLoader classLoader = Launcher.class.getClassLoader();
-        System.out.println(classLoader); //null
-    }
-}
+//package io.github.harvies.charon.tests.base.jvm.load;
+//
+//import sun.misc.Launcher;
+//
+////jdk9被移除
+//public class LauncherTest {
+//    public static void main(String[] args) {
+//        ClassLoader classLoader = Launcher.class.getClassLoader();
+//        System.out.println(classLoader); //null
+//    }
+//}
