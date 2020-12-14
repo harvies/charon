b1fa5d9a945c998ca81302c27ef4bb0f65f11ff1
harvies <cr0n6e@gmail.com>
2020-12-14 10:11:48 +0800

    On branch master Your branch is up to date with 'github/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   docker/nextcloud/nextcloud.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- docker/nextcloud/nextcloud.sh
+++ docker/nextcloud/nextcloud.sh
@@ -1,1 +1,1 @@
-docker run -d  --name nextcloud -p 808:80 --restart=always -v /data/nextcloud:/var/www/html -v /data2:/data2 harvies/nextcloud:latest
+docker run -d  --name nextcloud -p 808:80 --restart=always -v /data2/nextcloud:/var/www/html harvies/nextcloud:latest
