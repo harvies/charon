1369c6339be121c08a1fd9937800a1313fa871be
harvies <cr0n6e@gmail.com>
2020-12-27 20:21:21 +0800

    On branch master Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory) 	modified:   charon-tests/src/test/java/io/github/harvies/charon/tests/zk/WatchTest.java
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-tests/src/test/java/io/github/harvies/charon/tests/zk/WatchTest.java
+++ charon-tests/src/test/java/io/github/harvies/charon/tests/zk/WatchTest.java
@@ -1,23 +1,28 @@
 package io.github.harvies.charon.tests.zk;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.api.CuratorWatcher;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.locks.LockSupport;
 
 import static io.github.harvies.charon.tests.zk.ZookeeperTest.testPath;
 
 @Slf4j
 public class WatchTest {
+
+    private static final CuratorFramework client = ZkUtils.getClient();
+
     public static void main(String[] args) throws Exception {
         CuratorWatcher curatorWatcher = WatchTest::process;
-        ZkUtils.getClient().watchers().add().usingWatcher(curatorWatcher).forPath(testPath);
+        client.watchers().add().usingWatcher(curatorWatcher).forPath(testPath);
         LockSupport.park();
     }
 
-    private static void process(WatchedEvent event) {
+    private static void process(WatchedEvent event) throws Exception {
         log.info("event:[{}]", event);
         if (event.getType().getIntValue() == Watcher.Event.EventType.NodeCreated.getIntValue()) {
             log.info("节点[{}]被创建", event.getPath());
@@ -29,6 +34,8 @@ public class WatchTest {
         }
         if (event.getType().getIntValue() == Watcher.Event.EventType.NodeDataChanged.getIntValue()) {
             log.info("节点[{}]被更新", event.getPath());
+            byte[] bytes = client.getData().forPath(testPath);
+            log.info("getData:[{}]", new String(bytes, StandardCharsets.UTF_8));
         }
     }
 }
