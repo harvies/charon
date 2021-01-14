bfea8257a10b33881e659a6a159396eead4f1906
harvies <cr0n6e@gmail.com>
2021-01-14 10:57:36 +0800

    On branch master You are currently cherry-picking commit 26222ca.   (all conflicts fixed: run "git cherry-pick --continue")   (use "git cherry-pick --abort" to cancel the cherry-pick operation)
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-file/charon-excel/src/test/java/io/github/harvies/charon/excel/ConverterTest.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-file/charon-excel/src/test/java/io/github/harvies/charon/excel/ConverterTest.java
+++ charon-file/charon-excel/src/test/java/io/github/harvies/charon/excel/ConverterTest.java
@@ -4,8 +4,11 @@ import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import io.github.harvies.charon.util.FileUtils;
 import lombok.Data;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,8 +23,30 @@ public class ConverterTest {
         head.setB(Arrays.asList("ccccccccc", "dddddddd", "ccccccccc", "dddddddd", "ccccccccc", "dddddddd", "ccccccccc", "dddddddd"));
         head.setC(Arrays.asList(111111111, 1222222222, 1222222222, 1222222222, 1222222222, 1222222222, 1222222222));
         list.add(head);
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 背景设置为红色
+//        headWriteCellStyle.setFillForegroundColor(IndexedColors.ORANGE.getIndex());
+//        WriteFont headWriteFont = new WriteFont();
+//        headWriteFont.setFontHeightInPoints((short) 20);
+//        headWriteCellStyle.setWriteFont(headWriteFont);
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+//        contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+//        // 背景绿色
+//        contentWriteCellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+//        WriteFont contentWriteFont = new WriteFont();
+//        // 字体大小
+//        contentWriteFont.setFontHeightInPoints((short) 20);
+//        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        //单元格不溢出
+        contentWriteCellStyle.setHorizontalAlignment(HorizontalAlignment.FILL);
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy =
+                new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
         ExcelWriter excelWriter = EasyExcelFactory.write(FileUtils.getCurrentUserHomePath() + "/Downloads/test_write.xlsx")
                 .head(Head.class)
+                .registerWriteHandler(horizontalCellStyleStrategy)
                 .build();
         WriteSheet writeSheet = new WriteSheet();
         writeSheet.setSheetNo(0);
