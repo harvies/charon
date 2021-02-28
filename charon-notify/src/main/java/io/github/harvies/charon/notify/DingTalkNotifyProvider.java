968dbd9aa5e6e72071252e1da60964cdbbf9811d
harvies <cr0n6e@gmail.com>
2021-02-28 21:06:31 +0800

    notify

--- charon-notify/src/main/java/io/github/harvies/charon/notify/DingTalkNotifyProvider.java
+++ charon-notify/src/main/java/io/github/harvies/charon/notify/DingTalkNotifyProvider.java
@@ -28,23 +28,28 @@ public class DingTalkNotifyProvider implements NotifyProvider {
 
     @Override
     public boolean send(String title, String text) {
-        Map<String, Object> data = new HashMap<>();
-        data.put("msgtype", "markdown");
-        Map<String, Object> textObject = new HashMap<>();
-        textObject.put("title", title);
-        textObject.put("text", text);
-        data.put("markdown", textObject);
-        Map<String, String> headerMap = new HashMap<>();
-        headerMap.put("Content-Type", "application/json");
-        String result = Requests.post(dingTalkProperties.getWebHookUrl())
-                .body(JsonUtils.toJSONString(data))
-                .headers(headerMap)
-                .send().readToText();
-        JSONObject jsonObject = JsonUtils.parseObject(result);
-        log.info("dingtalk send result:[{}]", result);
-        if (jsonObject == null) {
+        try {
+            Map<String, Object> data = new HashMap<>();
+            data.put("msgtype", "markdown");
+            Map<String, Object> textObject = new HashMap<>();
+            textObject.put("title", title);
+            textObject.put("text", text);
+            data.put("markdown", textObject);
+            Map<String, String> headerMap = new HashMap<>();
+            headerMap.put("Content-Type", "application/json");
+            String result = Requests.post(dingTalkProperties.getWebHookUrl())
+                    .body(JsonUtils.toJSONString(data))
+                    .headers(headerMap)
+                    .send().readToText();
+            JSONObject jsonObject = JsonUtils.parseObject(result);
+            log.info("dingtalk send result:[{}]", result);
+            if (jsonObject == null) {
+                return false;
+            }
+            return Objects.equals(jsonObject.getInteger("errcode"), 0);
+        } catch (Exception e) {
+            log.warn("dingtalk send failed title:[{}] text:[{}]", title, text, e);
             return false;
         }
-        return Objects.equals(jsonObject.getInteger("errcode"), 0);
     }
 }
