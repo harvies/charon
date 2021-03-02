da8419b7ac65f82ccd8a0166a38d2f98351d3dc2
harvies <cr0n6e@gmail.com>
2021-03-02 17:35:46 +0800

    On branch master Changes to be committed:   (use "git restore --staged <file>..." to unstage) 	new file:   charon-tests/src/main/java/io/github/harvies/charon/tests/zk/ZkMain.java
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-tests/src/main/java/io/github/harvies/charon/tests/zk/ZkMain.java

---
+++ charon-tests/src/main/java/io/github/harvies/charon/tests/zk/ZkMain.java
@@ -0,0 +1,9 @@
+package io.github.harvies.charon.tests.zk;
+
+import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+
+public class ZkMain {
+    public static void main(String[] args) {
+        QuorumPeerMain.main(args);
+    }
+}
