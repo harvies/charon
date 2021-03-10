8333a067ddeb012beebaf22219e5ddf1f7403065
harvies <cr0n6e@gmail.com>
2021-03-11 00:28:51 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-api/README.md
    	deleted:    charon-attach/README.md
    	deleted:    charon-attach/pom.xml
    	deleted:    charon-attach/src/test/java/io/github/harvies/charon/attach/VirtualMachineTests.java
    	new file:   charon-config/charon-config-client/README.md
    	deleted:    charon-config/charon-config-server/pom.xml
    	renamed:    charon-json/src/main/java/io/github/harvies/charon/util/JsonUtils.java -> charon-core/src/main/java/io/github/harvies/charon/util/JsonUtils.java
    	deleted:    charon-json/pom.xml
    	new file:   charon-tests/src/test/java/io/github/harvies/charon/tests/attach/VirtualMachineTests.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-agent/pom.xml
    	modified:   charon-api/README.md
    	modified:   charon-api/pom.xml
    	modified:   charon-config/charon-config-client/README.md
    	modified:   charon-config/pom.xml
    	modified:   charon-core/pom.xml
    	modified:   charon-dependencies/pom.xml
    	modified:   charon-dlock/README.md
    	modified:   charon-dubbo/pom.xml
    	modified:   charon-extra/pom.xml
    	modified:   charon-notify/pom.xml
    	modified:   charon-spring-boot-starters/charon-web-spring-boot-starter/pom.xml
    	modified:   pom.xml

---
+++ charon-tests/src/test/java/io/github/harvies/charon/tests/attach/VirtualMachineTests.java
@@ -0,0 +1,24 @@
+package io.github.harvies.charon.tests.attach;
+
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class VirtualMachineTests {
+    @Test
+    public void list() {
+        List<VirtualMachineDescriptor> virtualMachineDescriptorList = VirtualMachine.list();
+        virtualMachineDescriptorList.forEach(System.out::println);
+    }
+
+    @Test
+    public void attach() throws IOException, AttachNotSupportedException {
+        VirtualMachine virtualMachine = VirtualMachine.attach("65920");
+        System.err.println(virtualMachine.getAgentProperties());
+        System.err.println(virtualMachine.getSystemProperties());
+    }
+}
