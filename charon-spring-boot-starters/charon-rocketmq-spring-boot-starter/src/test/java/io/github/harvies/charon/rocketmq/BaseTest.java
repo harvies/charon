4350d64cee28a2375c0dc3f833a565c1821e5263
harvies <cr0n6e@gmail.com>
2021-03-13 22:23:07 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	renamed:    charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/producer/CharonRocketMQProducerSpringBootApplication.java -> charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/CharonRocketMQSpringBootApplication.java
    	deleted:    charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/consumer/CharonRocketMQConsumerSpringBootApplication.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/BaseTest.java
    	modified:   charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/CharonRocketMQSpringBootApplication.java

--- charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/BaseTest.java
+++ charon-spring-boot-starters/charon-rocketmq-spring-boot-starter/src/test/java/io/github/harvies/charon/rocketmq/BaseTest.java
@@ -1,13 +1,12 @@
 package io.github.harvies.charon.rocketmq;
 
-import io.github.harvies.charon.rocketmq.producer.CharonRocketMQProducerSpringBootApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(value = SpringExtension.class)
-@SpringBootTest(classes = CharonRocketMQProducerSpringBootApplication.class)
+@SpringBootTest(classes = CharonRocketMQSpringBootApplication.class)
 @Slf4j
 public abstract class BaseTest {
 }
