180369f5139ba7d91d545106bc318a1623317613
harvies <cr0n6e@gmail.com>
2021-04-11 02:45:47 +0800

    springmvc5.3跨域

--- charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/config/CorsConfig.java
+++ charon-spring-boot-starters/charon-web-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/web/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // 设置你要允许的网站域名，如果全允许则设为 *
-        config.addAllowedOrigin("*");
+//        config.allowedOrigins("*"); //5.3前
+        config.addAllowedOriginPattern("*");
         // 如果要限制 HEADER 或 METHOD 请自行更改
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
