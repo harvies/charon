c6a39acf7db65fc1cdcc3fd898d9de114f07f3fe
harvies <cr0n6e@gmail.com>
2021-05-04 10:17:29 +0800

    On branch dev Your branch is up to date with 'origin/dev'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   docker/jdk/Dockerfile
    	modified:   docker/jdk/run.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- docker/jdk/run.sh
+++ docker/jdk/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm registry.cn-hangzhou.aliyuncs.com/harvies/jdk:latest /bin/bash
+docker run -it --rm registry.cn-hangzhou.aliyuncs.com/harvies/jdk:20210503 /bin/bash
