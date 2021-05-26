4edb1cb880eadb8584077ffc30c07735f1826689
harvies <delverjw@gmail.com>
2021-05-26 22:21:10 +0800

    On branch dev Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
    	new file:   charon-agent/src/test/java/io/github/harvies/charon/agent/JolTest.java
    	new file:   charon-core/src/test/java/io/github/harvies/charon/util/base/EqualsTest.java
    	new file:   charon-core/src/test/java/io/github/harvies/charon/util/collection/guava/MapsTest.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   charon-agent/pom.xml
    	modified:   charon-agent/src/test/java/io/github/harvies/charon/agent/JolTest.java
    	modified:   charon-core/src/test/java/io/github/harvies/charon/util/base/EqualsTest.java
    	modified:   charon-core/src/test/java/io/github/harvies/charon/util/collection/guava/MapsTest.java

---
+++ charon-core/src/test/java/io/github/harvies/charon/util/collection/guava/MapsTest.java
@@ -0,0 +1,78 @@
+package io.github.harvies.charon.util.collection.guava;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Maps;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.commons.collections4.ListValuedMap;
+import org.apache.commons.collections4.MultiMapUtils;
+import org.apache.commons.collections4.SetValuedMap;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author harvies
+ */
+public class MapsTest {
+    @Test
+    public void test1() {
+        List<User> list = new ArrayList<>();
+        list.add(User.builder().id(1L).username("aaa").build());
+        list.add(User.builder().id(2L).username("bbb").build());
+
+        Map<Long, User> userMap = Maps.uniqueIndex(list, User::getId);
+        System.out.println(userMap);
+    }
+
+    @Test
+    public void setValuedMap() {
+        SetValuedMap<Integer, Integer> map = MultiMapUtils.newSetValuedHashMap();
+        for (int i = 0; i < 10; i++) {
+            map.put(i, i + 100);
+            map.put(i, i + 200);
+        }
+        for (Integer key :
+                map.keySet()) {
+            System.err.println(key + "," + map.get(key));
+        }
+    }
+
+    @Test
+    public void listValuedMap() {
+        ListValuedMap<Integer, Integer> map = MultiMapUtils.newListValuedHashMap();
+        for (int i = 0; i < 10; i++) {
+            map.put(i, i + 100);
+            map.put(i, i + 200);
+        }
+        for (Integer key :
+                map.keySet()) {
+            System.err.println(key + "," + map.get(key));
+        }
+    }
+
+    @Test
+    public void arrayListMultimapTest() {
+        ArrayListMultimap<Integer, User> listMultimap = ArrayListMultimap.create();
+        for (int i = 0; i < 3; i++) {
+            int nextInt = RandomUtils.nextInt(1, 10);
+            System.err.println("nextInt:" + nextInt);
+            for (int j = 0; j < nextInt; j++) {
+                listMultimap.put(i, User.builder().id((long) i).username("username" + i).build());
+            }
+        }
+        System.err.println(listMultimap.keys());
+        System.err.println(listMultimap.size());
+    }
+
+    @Builder
+    @Data
+    private static class User {
+        private Long id;
+        private String username;
+    }
+
+}
