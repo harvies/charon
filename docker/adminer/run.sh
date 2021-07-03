309ab6403acd4986392bd4149d2bc36ce181b3ed
harvies <cr0n6e@gmail.com>
2021-07-04 00:39:29 +0800

    Update run.sh

--- docker/adminer/run.sh
+++ docker/adminer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 83:8080 registry.cn-hangzhou.aliyuncs.com/harvies/adminer:latest
+docker run -it -d  -p 83:8080 --name adminer  registry.cn-hangzhou.aliyuncs.com/harvies/adminer:latest
