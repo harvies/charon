7b6c7dcc9bd7be81d05e50006b2673dc2a59b214
harvies <delverjw@gmail.com>
2021-07-14 11:33:56 +0800

    robin

--- charon-spring-cloud-starters/charon-ribbon-spring-cloud-starter/src/main/java/io/github/harvies/charon/ribbon/RibbonServerListFilterConfiguration.java
+++ charon-spring-cloud-starters/charon-ribbon-spring-cloud-starter/src/main/java/io/github/harvies/charon/ribbon/RibbonServerListFilterConfiguration.java
@@ -1,21 +1,12 @@
 package io.github.harvies.charon.ribbon;
 
-import com.netflix.client.config.IClientConfig;
-import com.netflix.loadbalancer.Server;
-import com.netflix.loadbalancer.ServerListFilter;
-import org.springframework.cloud.netflix.ribbon.ZonePreferenceServerListFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class RibbonServerListFilterConfiguration {
-
-    // TODO: 2021/7/14 IClientConfig iClientConfig
     @Bean
-    public ServerListFilter<Server> ribbonServerListFilter() {
-        CharonServerListFilter filter = new CharonServerListFilter();
-//        filter.initWithNiwsConfig(iClientConfig);
-        return filter;
+    public CharonRule charonRule() {
+        return new CharonRule();
     }
 }
