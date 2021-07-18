67010a911602dc462d88ca51696bff9ea7650ddc
harvies <delverjw@gmail.com>
2021-07-18 12:57:32 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   charon-spring-cloud-starters/charon-gateway-spring-cloud-starter/src/main/java/io/github/harvies/charon/gateway/PersistenceRouteDefinitionRepository.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-spring-cloud-starters/charon-gateway-spring-cloud-starter/src/main/java/io/github/harvies/charon/gateway/PersistenceRouteDefinitionRepository.java
+++ charon-spring-cloud-starters/charon-gateway-spring-cloud-starter/src/main/java/io/github/harvies/charon/gateway/PersistenceRouteDefinitionRepository.java
@@ -1,5 +1,6 @@
 package io.github.harvies.charon.gateway;
 
+import java.util.Collections;
 import java.util.List;
 
 import com.alibaba.cloud.nacos.NacosConfigProperties;
@@ -7,6 +8,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.config.ConfigService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
@@ -34,6 +36,10 @@ public class PersistenceRouteDefinitionRepository implements RouteDefinitionRepo
     public Flux<RouteDefinition> getRouteDefinitions() {
         String dataId = applicationName + "-route-rule-" + activeProfiles;
         String config = configService.getConfig(dataId, nacosConfigProperties.getGroup(), 5000);
+        if (StringUtils.isBlank(config)) {
+            log.info("从nacos没有拉取到配置 dataId:[{}] group:[{}]", dataId, nacosConfigProperties.getGroup());
+            return Flux.fromIterable(Collections.emptyList());
+        }
         log.info("从nacos拉取最新的网关路由规则 dataId:[{}] group:[{}] config:[{}]", dataId, nacosConfigProperties.getGroup(), config);
         List<RouteDefinition> routeDefinitions = JSON.parseArray(config, RouteDefinition.class);
         return Flux.fromIterable(routeDefinitions);
