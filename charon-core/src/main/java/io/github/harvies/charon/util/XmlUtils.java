cd31ae7e40df799f9395c3ddb93089acd0c980eb
harvies <delverjw@gmail.com>
2021-09-11 23:08:49 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-core/src/main/java/io/github/harvies/charon/util/XmlUtils.java
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-core/src/main/java/io/github/harvies/charon/util/XmlUtils.java
    	modified:   charon-http/src/test/java/io/github/harvies/charon/http/RssTest.java

---
+++ charon-core/src/main/java/io/github/harvies/charon/util/XmlUtils.java
@@ -0,0 +1,15 @@
+package io.github.harvies.charon.util;
+
+public class XmlUtils {
+
+    /**
+     * 清理xml特殊字符
+     */
+    public static String clean(String xml) {
+        //清理头部空格
+        xml = xml.replaceAll(StringUtils.substringBefore(xml, "<?xml"), "");
+        //清理特殊字符
+        xml = xml.replaceAll("[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]", "");
+        return xml;
+    }
+}
--- charon-http/src/test/java/io/github/harvies/charon/http/RssTest.java
+++ charon-http/src/test/java/io/github/harvies/charon/http/RssTest.java
@@ -2,6 +2,8 @@ package io.github.harvies.charon.http;
 
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
+import io.github.harvies.charon.util.StringUtils;
+import io.github.harvies.charon.util.XmlUtils;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import net.dongliu.requests.Requests;
@@ -31,7 +33,8 @@ public class RssTest {
         SyndFeedInput syndFeedInput = new SyndFeedInput();
         SAXReader reader = new SAXReader();
         //https://blog.csdn.net/phoenix2121/article/details/6958176
-        @Cleanup InputStream inputStream = IOUtils.toInputStream(Requests.get("https://tech.youzan.com/rss/").send().readToText().replaceAll("[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]", ""), Charset.defaultCharset());
+        String text = Requests.get("https://ifeve.com/feed/").send().readToText();
+        @Cleanup InputStream inputStream = IOUtils.toInputStream(XmlUtils.clean(text), Charset.defaultCharset());
         Document document = reader.read(inputStream);
         StringReader stringReader = new StringReader(document.asXML());
         SyndFeed syndFeed = syndFeedInput.build(stringReader);
