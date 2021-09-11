c039d46e63302e8d19a46873bdf2b0a166be784f
harvies <delverjw@gmail.com>
2021-09-11 23:10:31 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-core/src/main/java/io/github/harvies/charon/util/XmlUtils.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-core/src/main/java/io/github/harvies/charon/util/XmlUtils.java
+++ charon-core/src/main/java/io/github/harvies/charon/util/XmlUtils.java
@@ -6,6 +6,9 @@ public class XmlUtils {
      * 清理xml特殊字符
      */
     public static String clean(String xml) {
+        if (StringUtils.isBlank(xml)) {
+            return xml;
+        }
         //清理头部空格
         xml = xml.replaceAll(StringUtils.substringBefore(xml, "<?xml"), "");
         //清理特殊字符
