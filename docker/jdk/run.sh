f70530e288a0c5d40850d73d38b36f42b98642ce
harvies <delverjw@gmail.com>
2021-09-12 18:32:42 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-dependencies/pom.xml
    	modified:   docker/jdk/Dockerfile
    	modified:   docker/jdk/build.sh
    	modified:   docker/jdk/push.sh
    	modified:   docker/jdk/run.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- docker/jdk/run.sh
+++ docker/jdk/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm registry.cn-hangzhou.aliyuncs.com/harvies/jdk:20210504 /bin/bash
+docker run -it --rm registry.cn-hangzhou.aliyuncs.com/harvies/jdk:20210912 /bin/bash
