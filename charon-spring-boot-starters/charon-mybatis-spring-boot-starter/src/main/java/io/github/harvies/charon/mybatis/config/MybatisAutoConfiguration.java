943236f369a5174ca1c23e5cffea48cd61e0075a
harvies <delverjw@gmail.com>
2021-09-19 21:00:00 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
    	new file:   charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/resources/META-INF/spring.factories
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
    	modified:   charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/resources/META-INF/spring.factories

---
+++ charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
@@ -0,0 +1,21 @@
+package io.github.harvies.charon.mybatis.config;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisAutoConfiguration {
+
+    @Bean
+    public PaginationInnerInterceptor paginationInterceptor() {
+        PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setOptimizeJoin(true);
+        return paginationInterceptor;
+    }
+}
