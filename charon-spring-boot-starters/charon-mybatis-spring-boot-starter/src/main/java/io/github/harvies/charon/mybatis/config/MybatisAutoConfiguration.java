f3bfd2b86cd6c12675e416bc24c2a0381f647356
harvies <delverjw@gmail.com>
2021-09-21 16:59:33 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
+++ charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
@@ -1,6 +1,5 @@
 package io.github.harvies.charon.mybatis.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
