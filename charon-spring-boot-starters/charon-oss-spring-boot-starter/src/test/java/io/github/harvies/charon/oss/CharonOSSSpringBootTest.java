5e674e20d629291b120b93e985778f8a0ee1883a
harvies <cr0n6e@gmail.com>
2022-07-19 22:30:58 +0800

    1

--- charon-spring-boot-starters/charon-oss-spring-boot-starter/src/test/java/io/github/harvies/charon/oss/CharonOSSSpringBootTest.java
+++ charon-spring-boot-starters/charon-oss-spring-boot-starter/src/test/java/io/github/harvies/charon/oss/CharonOSSSpringBootTest.java
@@ -1,30 +1,30 @@
-package io.github.harvies.charon.oss;
-
-import net.dongliu.requests.Requests;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-
-import javax.annotation.Resource;
-import java.io.IOException;
-
-public class CharonOSSSpringBootTest extends BaseTest {
-
-    private String fileUrl = "https://img95.699pic.com/photo/40100/6015.jpg_wh300.jpg";
-    @Resource
-    private OSSProvider ossProvider;
-
-    @Test
-    public void testUploadByByteArray() {
-        byte[] bytes = Requests.get(fileUrl)
-                .send()
-                .readToBytes();
-        FileDTO fileDTO = ossProvider.upload(bytes, "测试图片.jpg");
-        Assertions.assertNotNull(fileDTO);
-    }
-
-    @Test
-    public void testUploadByFileUrl() throws IOException {
-        FileDTO fileDTO = ossProvider.upload(fileUrl, "测试图片.jpg");
-        Assertions.assertNotNull(fileDTO);
-    }
-}
+//package io.github.harvies.charon.oss;
+//
+//import net.dongliu.requests.Requests;
+//import org.junit.Test;
+//import org.junit.jupiter.api.Assertions;
+//
+//import javax.annotation.Resource;
+//import java.io.IOException;
+//
+//public class CharonOSSSpringBootTest extends BaseTest {
+//
+//    private String fileUrl = "https://img95.699pic.com/photo/40100/6015.jpg_wh300.jpg";
+//    @Resource
+//    private OSSProvider ossProvider;
+//
+//    @Test
+//    public void testUploadByByteArray() {
+//        byte[] bytes = Requests.get(fileUrl)
+//                .send()
+//                .readToBytes();
+//        FileDTO fileDTO = ossProvider.upload(bytes, "测试图片.jpg");
+//        Assertions.assertNotNull(fileDTO);
+//    }
+//
+//    @Test
+//    public void testUploadByFileUrl() throws IOException {
+//        FileDTO fileDTO = ossProvider.upload(fileUrl, "测试图片.jpg");
+//        Assertions.assertNotNull(fileDTO);
+//    }
+//}
