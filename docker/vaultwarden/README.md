ca28a622fe4c6a2fd9a3aa60585d1be6a07518a7
harvies <cr0n6e@gmail.com>
2022-07-30 19:35:41 +0800

    1

--- docker/vaultwarden/README.md
+++ docker/vaultwarden/README.md
@@ -1,2 +1,1 @@
-docker pull vaultwarden/server:latest
-docker run -d --name vaultwarden -v /volume2/docker/vaultwarden:/data/ -p 84:80  --restart=always vaultwarden/server:latest
+docker run -d --name vaultwarden -v /home/vaultwarden:/data/ -p 84:80  --restart=always vaultwarden/server:latest
--- docker/wiznote/deploy.sh
+++ docker/wiznote/deploy.sh
@@ -1,1 +1,20 @@
-docker run --name wiz --restart=always -it -d -v  /volume3/wizdata:/wiz/storage -v  /etc/localtime:/etc/localtime -p 83:80 -p 9269:9269/udp  wiznote/wizserver
+3步部署
+
+1、
+
+docker run --name wiz --restart=always -it -d -v  /home/wizdata:/wiz/storage -v  /etc/localtime:/etc/localtime -p 83:80 -p 9269:9269/udp  wiznote/wizserver
+
+2、
+docker exec -it wiz bash
+cd /wiz/app/wizserver/node_modules/node-rsa/src/
+rm -rf NodeRSA.js
+wget https://raw.githubusercontent.com/yao177/sillygirl-plugin/main/NodeRSA.js
+exit
+docker restart wiz
+
+3、
+你的IP地址+8848访问。
+
+结束
+
+http://124.222.129.205/?p=446
