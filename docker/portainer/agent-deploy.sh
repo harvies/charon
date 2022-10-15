67a53d6f6690e1838652a3ff7b9c8a554efa4d6e
harvies <cr0n6e@gmail.com>
2022-10-15 14:46:27 +0800

    111

---
+++ docker/portainer/agent-deploy.sh
@@ -0,0 +1,1 @@
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /volume1/@docker/volumes:/var/lib/docker/volumes portainer/agent:latest
