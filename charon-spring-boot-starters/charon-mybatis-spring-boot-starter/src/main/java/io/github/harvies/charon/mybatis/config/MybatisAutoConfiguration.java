a085ba1ed8aa99bb8a6d7a43d441a8b7ea219de5
harvies <cr0n6e@gmail.com>
2022-10-17 23:23:47 +0800

    111

--- charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
+++ charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/config/MybatisAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.github.harvies.charon.mybatis.config;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import io.github.harvies.charon.mybatis.injector.MySqlInjector;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +26,7 @@ public class MybatisAutoConfiguration {
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
      */
     @Bean
+    @ConditionalOnMissingBean
     public MybatisPlusInterceptor mybatisPlusInterceptor(PaginationInnerInterceptor paginationInnerInterceptor) {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(paginationInnerInterceptor);
