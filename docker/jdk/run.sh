8e4b351f0135e64847bd231585990241f6511356
harvies <cr0n6e@gmail.com>
2022-10-17 22:25:46 +0800

    111

--- docker/jdk/run.sh
+++ docker/jdk/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm registry.cn-hangzhou.aliyuncs.com/harvies/jdk:20210912 /bin/bash
+docker run -it --rm registry.cn-hangzhou.aliyuncs.com/harvies/jdk:20221017 /bin/bash
