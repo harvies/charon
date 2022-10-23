32829aa29033ddd0bd6e121301acc3dcefd767b0
harvies <cr0n6e@gmail.com>
2022-10-23 22:37:22 +0800

    1

---
+++ charon-jcef/src/test/java/com/jingheng/plugin/domain/com/jingheng/plugin/utils/CefHttpClient.java
@@ -0,0 +1,126 @@
+//package com.jingheng.plugin.domain.com.jingheng.plugin.utils;
+//
+//import com.jingheng.plugin.common.domain.SupplyResult;
+//import com.jingheng.plugin.common.utils.CharsetUtil;
+//import com.jingheng.plugin.domain.CefHttpResponse;
+//import org.apache.commons.io.IOUtils;
+//import org.cef.callback.CefAuthCallback;
+//import org.cef.callback.CefURLRequestClient;
+//import org.cef.network.CefRequest;
+//import org.cef.network.CefURLRequest;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//import java.io.UnsupportedEncodingException;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//public class CefHttpClient {
+//
+//    private static Logger logger = LoggerFactory.getLogger(CefHttpClient.class);
+//    private final static ConcurrentHashMap<Integer, CefHttpResponse> resultMap = new ConcurrentHashMap();
+//    private final static long readTimeout = 60000;
+//    public static CefHttpClient INSTANCE = new CefHttpClient();
+//
+//    public SupplyResult<String> execute(CefRequest request, String charset) {
+//        SupplyResult<String> supplyResult = new SupplyResult<>();
+//        try {
+//            HttpRequestTask httpRequestTask = new HttpRequestTask(request, charset);
+//            httpRequestTask.execute();
+//            long beginTime = System.currentTimeMillis();
+//            while (System.currentTimeMillis() - beginTime < readTimeout) {
+//                CefHttpResponse result = resultMap.get(request.hashCode());
+//                if (result != null) {
+//                    CefURLRequest.Status requestStatus = result.getCefURLRequest().getRequestStatus();
+//                    if (CefURLRequest.Status.UR_SUCCESS.equals(requestStatus)) {
+//                        supplyResult.setStatus(SupplyResult.STATUS_SUCCESS);
+//                        supplyResult.setModule(result.getResposeStr());
+//                    } else if (CefURLRequest.Status.UR_FAILED.equals(requestStatus)) {
+//                        supplyResult.setStatus(SupplyResult.STATUS_FAILED);
+//                    }
+//                    break;
+//                }
+//                Thread.sleep(100);
+//            }
+//            resultMap.remove(request.hashCode());
+//        } catch (Exception e) {
+//            logger.warn("", e);
+//        }
+//        return supplyResult;
+//    }
+//
+//
+//    private class HttpRequestTask implements CefURLRequestClient {
+//        private CefRequest request;
+//        private String charset;
+//        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//
+//        public HttpRequestTask(CefRequest request, String charset) {
+//            this.charset = charset;
+//            this.request = request;
+//        }
+//
+//        @Override
+//        public void onRequestComplete(CefURLRequest cefURLRequest) {
+//            CefHttpResponse cefHttpResponse = new CefHttpResponse();
+//            cefHttpResponse.setCefURLRequest(cefURLRequest);
+//            byte[] bytes = os.toByteArray();
+//            if (charset != null) {
+//                try {
+//                    cefHttpResponse.setResposeStr(new String(bytes, charset));
+//                } catch (UnsupportedEncodingException e) {
+//                    logger.warn("", e);
+//                }
+//            } else {
+//                String ioEncode = CharsetUtil.getIOEncode(new ByteArrayInputStream(bytes), bytes.length);
+//                try {
+//                    cefHttpResponse.setResposeStr(new String(bytes, ioEncode));
+//                } catch (UnsupportedEncodingException e) {
+//                    logger.warn("", e);
+//                }
+//            }
+//            resultMap.put(request.hashCode(), cefHttpResponse);
+//        }
+//
+//        @Override
+//        public void onUploadProgress(CefURLRequest cefURLRequest, int i, int i1) {
+//
+//        }
+//
+//        @Override
+//        public void onDownloadProgress(CefURLRequest cefURLRequest, int i, int i1) {
+//
+//        }
+//
+//        @Override
+//        public void onDownloadData(CefURLRequest cefURLRequest, byte[] bytes, int i) {
+//            try {
+//                IOUtils.write(bytes, os);
+//            } catch (IOException e) {
+//                logger.warn("", e);
+//            }
+//        }
+//
+//        @Override
+//        public boolean getAuthCredentials(boolean b, String s, int i, String s1, String s2, CefAuthCallback cefAuthCallback) {
+//            return false;
+//        }
+//
+//        @Override
+//        public void setNativeRef(String s, long l) {
+//
+//        }
+//
+//        @Override
+//        public long getNativeRef(String s) {
+//            return 0;
+//        }
+//
+//        public void execute() {
+//            CefURLRequest.create(request, this);
+//        }
+//    }
+//
+//}
