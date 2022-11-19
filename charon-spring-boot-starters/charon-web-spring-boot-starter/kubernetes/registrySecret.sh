3fc30440579c8bdc8b095733023903896a851a7e
harvies <cr0n6e@gmail.com>
2022-11-20 02:34:39 +0800

    1

---
+++ charon-spring-boot-starters/charon-web-spring-boot-starter/kubernetes/registrySecret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry myregistrykey \
+--docker-server=DOCKER_REGISTRY_SERVER \
+--docker-username=DOCKER_USER \
+--docker-password=DOCKER_PASSWORD \
+--docker-email=DOCKER_EMAIL
