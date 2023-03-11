fea5bb001f4b5a039b56ce3a4c80cfa1e1ef0f3f
harvies <delverjw@gmail.com>
2023-03-11 06:51:35 +0000

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   shell/vp9_to_h265.sh

--- shell/vp9_to_h265.sh
+++ shell/vp9_to_h265.sh
@@ -3,19 +3,15 @@
 # usage
 # ./vp9_to_h265.sh input.vp9 output.mp4
 
-# 定义输入文件名和输出文件名
-input_file="input.vp9"
-output_file="output.mp4"
-
-# 如果命令行参数中指定了输入文件名，则使用命令行参数的值
-if [ "$#" -gt 0 ]; then
-  input_file="$1"
+# 检查命令行参数
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 input_file [output_file]"
+  exit 1
 fi
 
-# 如果命令行参数中指定了输出文件名，则使用命令行参数的值
-if [ "$#" -gt 1 ]; then
-  output_file="$2"
-fi
+# 获取输入文件名和输出文件名
+input_file="$1"
+output_file="${2:-${input_file%.*}.mp4}"
 
 # 转码命令
 ffmpeg -i "$input_file" -c:v libx265 -crf 0 -preset ultrafast "$output_file"
