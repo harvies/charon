2acfd7db9fbe0ef1e6649389a41f594f72c78f88
harvies <cr0n6e@gmail.com>
2023-03-20 13:26:44 +0800

    1

--- package.sh
+++ package.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+source ./set_jdk17.sh
 mvn clean package  -U  -DskipTests
---
+++ set_jdk17.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 判断操作系统类型
+OS_TYPE=$(uname -s)
+if [ $OS_TYPE = "Darwin" ]; then
+  # Mac系统
+  export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+elif [ $OS_TYPE = "Linux" ]; then
+  # Linux系统
+  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+else
+  # Windows系统
+  export JAVA_HOME="C:\Program Files\Java\jdk-17"
+fi
+
+# 将JDK的bin目录添加到系统的PATH环境变量中
+export PATH=$JAVA_HOME/bin:$PATH
+
+# 打印JDK版本
+java -version > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Java环境变量设置成功，当前Java版本为："
+  java -version
+else
+  echo "Java环境变量设置失败，请检查Java安装路径是否正确"
+fi
