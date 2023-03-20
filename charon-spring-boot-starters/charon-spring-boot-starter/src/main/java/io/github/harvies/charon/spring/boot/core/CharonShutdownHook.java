d1a422bc4071a6de89b94f22c6b8712ef4300812
harvies <cr0n6e@gmail.com>
2023-03-20 23:59:50 +0800

    1

--- charon-spring-boot-starters/charon-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/core/CharonShutdownHook.java
+++ charon-spring-boot-starters/charon-spring-boot-starter/src/main/java/io/github/harvies/charon/spring/boot/core/CharonShutdownHook.java
@@ -3,23 +3,22 @@ package io.github.harvies.charon.spring.boot.core;
 import io.github.harvies.charon.spring.boot.core.event.CharonBootShutdownEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-
-import javax.annotation.Resource;
 
 @Slf4j
 public class CharonShutdownHook implements InitializingBean {
-    @Resource
-    private CharonBootApplicationProperties properties;
-    @Resource
+    @Autowired
+    private CharonBootApplicationProperties charonBootApplicationProperties;
+    @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public void afterPropertiesSet() {
         log.info("charon boot application register shutdown hook success!");
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            log.error("charon boot application shutdown:{}", properties);
-            applicationEventPublisher.publishEvent(new CharonBootShutdownEvent(properties));
+            log.error("charon boot application shutdown:{}", charonBootApplicationProperties);
+            applicationEventPublisher.publishEvent(new CharonBootShutdownEvent(charonBootApplicationProperties));
         }));
     }
 
