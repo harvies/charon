eac7b32fbd156708b2c141f694b2f3903dc94ffa
harvies <cr0n6e@gmail.com>
2023-03-21 23:50:23 +0800

    Revert "1"
    This reverts commit 29da282f7409ae668d91accc1d119cbb65ad8149.

---
+++ charon-api/src/main/java/io/github/harvies/charon/dto/BaseDTO.java
@@ -0,0 +1,7 @@
+package io.github.harvies.charon.dto;
+
+import java.io.Serializable;
+
+public abstract class BaseDTO implements Serializable {
+    private static final long serialVersionUID = -4721607536018568393L;
+}
