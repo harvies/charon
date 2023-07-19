a53abe2bb9525fae0e88a01268f4f243c1c8c9a1
harvies <cr0n6e@gmail.com>
2023-07-19 13:33:10 +0800

    1

--- charon-spring-boot-starters/charon-web-spring-boot-starter/src/test/java/io/github/harvies/test/spring/boot/web/CharonSpringBootWebApplication.java
+++ charon-spring-boot-starters/charon-web-spring-boot-starter/src/test/java/io/github/harvies/test/spring/boot/web/CharonSpringBootWebApplication.java
@@ -2,10 +2,13 @@ package io.github.harvies.test.spring.boot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class CharonSpringBootWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CharonSpringBootWebApplication.class, args);
+        SpringApplication app = new SpringApplication(CharonSpringBootWebApplication.class);
+        app.setApplicationStartup(new BufferingApplicationStartup(2048));
+        app.run(args);
     }
 }
