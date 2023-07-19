c955edf28ce261b76d498f8b2c059cd520953157
harvies <cr0n6e@gmail.com>
2023-07-19 21:34:16 +0800

    del

--- charon-spring-cloud-starters/charon-gray-spring-cloud-starter/README.md
+++
@@ -1,2 +0,0 @@
-## 灰度组，逗号分割
-green-group=xxx
--- charon-spring-cloud-starters/charon-gray-spring-cloud-starter/src/main/java/io/github/harvies/charon/gray/constant/CommonConstant.java
+++
@@ -1,9 +0,0 @@
-package io.github.harvies.charon.gray.constant;
-
-/**
- * @author harvies
- */
-public class CommonConstant {
-
-    public static final String VERSION = "1.0.0";
-}
