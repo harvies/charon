ac5faac1aa9e65e3a93b2c754e3eff933c35c2b5
harvies <cr0n6e@gmail.com>
2023-07-22 16:21:20 +0800

    del

--- charon-spring-boot-starters/charon-mybatis-spring-boot-starter/src/main/java/io/github/harvies/charon/mybatis/diff/DiffService.java
+++
@@ -1,9 +0,0 @@
-package io.github.harvies.charon.mybatis.diff;
-
-import java.util.List;
-
-public interface DiffService<DTO extends BasePlusDTO, PO extends BasePlusPO> {
-    DiffResult<DTO, PO> get(List<DTO> rList);
-
-    void persistence(DiffResult<DTO, PO> diffResult);
-}
