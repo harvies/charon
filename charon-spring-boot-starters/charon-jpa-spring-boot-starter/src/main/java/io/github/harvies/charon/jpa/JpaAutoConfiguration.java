38fe5396d8fef7d8c4be92b55bb0556eff9617ce
harvies <cr0n6e@gmail.com>
2024-06-21 23:06:25 +0800

    jpa

--- charon-spring-boot-starters/charon-jpa-spring-boot-starter/src/main/java/io/github/harvies/charon/jpa/JpaAutoConfiguration.java
+++ charon-spring-boot-starters/charon-jpa-spring-boot-starter/src/main/java/io/github/harvies/charon/jpa/JpaAutoConfiguration.java
@@ -1,18 +1,28 @@
 package io.github.harvies.charon.jpa;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
 import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class JpaConfiguration{
+@AutoConfiguration
+@Slf4j
+public class JpaAutoConfiguration {
 
     @Resource
     private EntityManager entityManager;
+
+    @PostConstruct
+    public void init() {
+        log.info("JpaAutoConfiguration inited");
+    }
+
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
+        log.info("JPAQueryFactory inited");
         return new JPAQueryFactory(entityManager);
     }
 
