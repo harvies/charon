1770ac4836e601255f072d355c612b9fa0d879f6
harvies <cr0n6e@gmail.com>
2024-06-21 22:26:09 +0800

    jpa

--- charon-spring-boot-starters/charon-jpa-spring-boot-starter/src/test/java/io/github/harvies/charon/jpa/CustomerRepository.java
+++ charon-spring-boot-starters/charon-jpa-spring-boot-starter/src/test/java/io/github/harvies/charon/jpa/CustomerRepository.java
@@ -1,8 +1,9 @@
 package io.github.harvies.charon.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long>, QuerydslPredicateExecutor<Customer> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, QuerydslPredicateExecutor<Customer>, JpaSpecificationExecutor<Customer> {
     // 可以定义自定义查询方法
 }
