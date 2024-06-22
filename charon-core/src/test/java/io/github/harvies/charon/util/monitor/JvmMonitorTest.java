a075370184b3ccab0a31775fe2c95c514f83d71a
harvies <cr0n6e@gmail.com>
2024-06-22 13:53:45 +0800

    jvm

--- charon-core/src/test/java/io/github/harvies/charon/util/monitor/JvmMonitorTest.java
+++ charon-core/src/test/java/io/github/harvies/charon/util/monitor/JvmMonitorTest.java
@@ -1,12 +1,27 @@
-package io.github.harvies.charon.tests.base.jvm.monitor;
+package io.github.harvies.charon.util.monitor;
+
+import org.junit.jupiter.api.Test;
 
 import java.lang.management.*;
 import java.util.List;
 
-public class JVM {
+public class JvmMonitorTest {
 
-    public static void main(String[] args) {
-        
+    @Test
+    public void test() {
+        //Java 虚拟机的运行时系统的管理接口。 RuntimeMXBean
+        RuntimeMXBean run = ManagementFactory.getRuntimeMXBean();
+        System.out.println("主机 " + run.getName());
+        System.out.println("Java 虚拟机规范名称 " + run.getSpecName());
+        System.out.println("返回 Java 库路径 " + run.getLibraryPath());
+        System.out.println("系统类加载器用于搜索类文件的 Java 类路径 " + run.getClassPath());
+        System.out.println("规范供应商 " + run.getSpecVendor());
+        System.out.println("虚拟机规范版本 " + run.getSpecVersion());
+        System.out.println("虚拟机参数 " + run.getInputArguments());
+        System.out.println("虚拟机名称 " + run.getVmName());
+        System.out.println("虚拟机版本 " + run.getVmVersion());
+        System.out.println("虚拟机供应商 " + run.getVmVendor());
+
         //Java 虚拟机线程系统的管理接口 ThreadMXBean
         ThreadMXBean th = ManagementFactory.getThreadMXBean();
         System.out.println("活动线程的当前数目" + th.getThreadCount());
@@ -45,7 +60,5 @@ public class JVM {
         System.out.println("Java 虚拟机开始执行到目前已经加载的类的总数" + cl.getTotalLoadedClassCount());
         System.out.println("Java 虚拟机开始执行到目前已经卸载的类的总数" + cl.getUnloadedClassCount());
 
-
     }
-
 }
