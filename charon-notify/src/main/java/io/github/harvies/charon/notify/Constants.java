f9b5fe1d210d9560874925472daf063ae0f9adab
harvies <cr0n6e@gmail.com>
2024-08-01 08:48:11 +0800

    删除oss notify

--- charon-notify/src/main/java/io/github/harvies/charon/notify/Constants.java
+++
@@ -1,9 +0,0 @@
-package io.github.harvies.charon.notify;
-
-public class Constants {
-    /**
-     * notify前缀
-     */
-    public static final String NOTIFY_PROPERTIES_PREFIX = "charon.notify";
-
-}
--- charon-notify/src/main/java/io/github/harvies/charon/notify/NotifyProvider.java
+++
@@ -1,6 +0,0 @@
-package io.github.harvies.charon.notify;
-
-public interface NotifyProvider {
-
-    boolean send(String title, String text);
-}
